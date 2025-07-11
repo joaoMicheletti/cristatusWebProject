5b59971456916e49c355c4fd37b4bf3a0a8ae5f2
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2025-07-11 12:50:26 -0300

    update
    update

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,9 +1,14 @@
-//import Icon from '../src/assets/image/logoLogin.webp';
-self.addEventListener('push', function(event){
+
+
+self.addEventListener('push', function(event) {
     const body = event.data?.text() ?? '';
+
     event.waitUntil(
-        self.registration.showNotification('Cristatus', {
+        self.registration.showNotification('Cristatus CS', {
             body,
+            icon: '/logo512.png', // Caminho relativo para o ícone
+            // Caso queira adicionar uma imagem maior (ex: logo grande), use o campo 'image'
+            //image: '/assets/image/logoLogin.webp',
         })
-    )
+    );
 });
